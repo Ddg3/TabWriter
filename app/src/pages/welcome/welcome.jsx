717ce517546dbc1297bf88a4c72436f012cc0de1
Ddg3 <zachlawrence2003@hotmail.com>
2022-02-07 12:43:24 -0800

    stable commit w extra links removed

--- app/src/pages/welcome/welcome.jsx
+++ app/src/pages/welcome/welcome.jsx
@@ -25,9 +25,6 @@ class Welcome extends React.Component {
             <h2 className="title is-2">Samples</h2>
             <div>
               <Link to={ROUTES.MOTD}>Using the Electron store.</Link> <br />
-              <Link to={ROUTES.LOCALIZATION}>Changing locales.</Link> <br />
-              <Link to={ROUTES.UNDOREDO}>Undo/redoing actions.</Link> <br />
-              <Link to={ROUTES.CONTEXTMENU}>Custom context menu.</Link> <br />
             </div>
           </div>
         </section>
