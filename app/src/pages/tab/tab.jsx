8c420945f0ede27478cdf34ae3807f2ca6143e18
Ddg3 <zachlawrence2003@hotmail.com>
2022-03-16 18:48:35 -0700

    added arrow movement around fields, dynaimc column creation

--- app/src/pages/tab/tab.jsx
+++ app/src/pages/tab/tab.jsx
@@ -2,44 +2,91 @@ import React from "react";
 import ROUTES from "Constants/routes";
 import { Link } from "react-router-dom";
 import "./tab.css";
+import Mousetrap from 'mousetrap';
 
 class Tab extends React.Component {
     constructor(props) {
       super(props);
-      this.state = { inputs: ["inputs-0", "inputs-1", "inputs-2", "inputs-3", "inputs-4", "inputs-5"] };
+      this.state = { 
+        strings: ["1","2","3","4","5","6"],
+        columns: ["inputs-0"],
+        position: 0,
+      };
+
+      this.focusText = null;
+      this.lastColumn = null;
+      this.setFocusRef = element =>{
+        this.focusText = element;
+      }
+
+      this.setLastColumnRef = element =>{
+        this.lastColumn = element;
+      }
+
+      this.handleKeys = this.handleKeys.bind(this)
+      this.copyLastColumn = this.copyLastColumn.bind(this)
+
+      var Mousetrap = require('mousetrap');
+      Mousetrap.prototype.stopCallback = function () {
+        return false;
+      }
+      Mousetrap.bind('up', (e) => this.handleKeys(e, -1))
+      Mousetrap.bind('down', (e) => this.handleKeys(e, 1))
+      Mousetrap.bind('left', (e) => this.handleKeys(e, -6))
+      Mousetrap.bind('right', (e) => this.handleKeys(e, 6))
+      Mousetrap.bind(['ctrl+d', 'command+d'], (e) => this.copyLastColumn(e))
     }
 
+    copyLastColumn(e){
+      e.preventDefault()
+      this.handleKeys(e, 6)
+      this.appendInput()
+    }
+    handleKeys(e, increment){
+      e.preventDefault()
+      if(this.state.position + increment >= 0)
+        this.setState(prevState => ({ position: prevState.position + increment}))
+    }
+
+    componentDidUpdate(){
+      let position = this.state.position;
+      let columns = this.state.columns.length;
+
+      if(position / 6 > columns - 1)
+        this.appendInput()
+
+      if(this.focusText)
+        this.focusText.focus();
+      
+    }
+
+    appendInput() {
+      var newInput = `input-${this.state.columns.length}`;
+      this.setState(prevState => ({ columns: prevState.columns.concat([newInput]) }));
+    }
     render() {
     return (
       <React.Fragment>
         <section className="section">
           <div className="input-container">
           {
-            this.state.inputs.map(() =>
-                <div className="input-field">
-                  <input type="number"></input>
-                </div>
+            this.state.columns.map((col, i) =>
+              <div className="input-column">
+                {
+                  this.state.strings.map((field, j) =>
+                    <div className="input-field">
+                      <input type="text" ref={(i*6)+j == this.state.position ? this.setFocusRef : null}></input>
+                    </div>
+                  )
+                }
+              </div>
             )
           }
           </div>
-          <button onClick={ () => this.appendInput() }>
-            ADD COLUMN
-          </button>
         </section>
       </React.Fragment>
     );
   }
-
-  appendInput() {
-    for(let i = 0; i < 6; i++){
-      var newInput = `input-${this.state.inputs.length}`;
-      this.setState(prevState => ({ inputs: prevState.inputs.concat([newInput]) }));
-    }
-  }
 }
-// const mapStateToProps = (state, props) => ({
-//     home: state.home,
-//   });
-// const mapDispatch = { changeMessage };
 
 export default Tab
